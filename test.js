d2a4c42afd1e18d86421f3e4c4053c80737f52fc
Iván Sánchez Ortega <ivan@sanchezortega.es>
2015-08-19 01:29:02 +0200

    Minor changes in default page template

--- test.js
+++ test.js
@@ -7,9 +7,13 @@ var doc = new LeafDoc();
 // doc.addFile('leafdoc.js');
 doc.addFile('../Leaflet/src/layer/marker/Marker.js');
 doc.addFile('../Leaflet/src/layer/marker/Marker.Drag.js');
+// doc.addFile('../Leaflet/src/layer/marker/Marker.Popup.js');
 doc.addFile('../Leaflet/src/layer/Popup.js');
 doc.addFile('../Leaflet/src/core/Events.js');
 doc.addFile('../Leaflet/src/layer/Layer.js');
+doc.addFile('../Leaflet/src/layer/tile/GridLayer.js');
+doc.addFile('../Leaflet/src/layer/tile/TileLayer.js');
+doc.addFile('../Leaflet/src/layer/tile/TileLayer.WMS.js');
 
 // console.log('internal namespaces are', doc._namespaces);
 
