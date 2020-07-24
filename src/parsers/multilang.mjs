660a464d7d4256db362a5337faded4197b0a1a69
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-07-24 11:24:00 +0200

    Update multilang-extract-comments version to support '.mjs' files.

--- src/parsers/multilang.mjs
+++ src/parsers/multilang.mjs
@@ -7,12 +7,7 @@ import mec from 'multilang-extract-comments';
 // version of the comments data structure
 
 export default function multilangParser(str, filename) {
-	// comment-patterns dependency of mec, doesn't have support for '.mjs' modules,
-	// see https://github.com/nknapp/comment-patterns/pull/9
-	if (!filename || path.extname(filename) === '.mjs') {
-		filename = 'leafdoc_tmp.js';
-	}
-	const mecBlocks = mec(str, {filename});
+	const mecBlocks = mec(str, {filename: filename || 'leafdoc_tmp.js'});
 	const blocks = Object.values(mecBlocks).map(mecBlock => mecBlock.content.trim()).filter(block => block && block !== '');
 
 	return blocks;
