00a0497f3bef8f42ab729e914376df7c675510db
Iván Sánchez Ortega <ivan@sanchezortega.es>
2020-09-08 18:31:22 +0200

    Add e2e tests for _ and $ JS identifiers

---
+++ spec/e2e/javascript-identifiers/identifiers.js
@@ -0,0 +1,8 @@
+// 🍂class $
+// jQuery stuff.
+// 🍂function _each(arr: Array, fn: Function): undefined
+// Returns a random number between 0.0 and 1.0
+export function _each(foo, bar) {
+	// Do something here.
+}
+
