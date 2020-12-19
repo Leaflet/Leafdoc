a849f76185013925496e1e357d13f747c9db6774
Iván Sánchez Ortega <ivan@sanchezortega.es>
2020-12-19 16:53:04 +0100

    Fix typos in the new e/empty CLI option

--- src/cli.mjs
+++ src/cli.mjs
@@ -36,10 +36,10 @@ const argv = minimist(process.argv.slice(2), {
 		o: 'output',
 		// 🍂option json: Boolean=false; Write the internal JSON representation of the documentation instead of a templated HTML file.
 		// 🍂option o; Alias of `output`
-		j: 'json'
+		j: 'json',
 		// 🍂option empty: Boolean=false; Akin to [Leafdoc.showInheritancesWhenEmpty](#leafdoc.showinheritanceswhenempty)
 		// 🍂option e; Alias of `empty`
-		e: 'json'
+		e: 'empty'
 	},
 	boolean: ['v', 'verbose', 'j', 'json'],
 	string: ['t', 'template', 'c', 'character'],
